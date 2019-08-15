6d5eefea8f0ec3c2cd017788a83a8a03d4a428d5
hoto17296 <mail@hoto.me>
2019-08-15 22:31:38 +0900

    fix dir

--- pytunes/__init__.py
+++ pytunes/__init__.py
@@ -3,13 +3,19 @@ import subprocess
 import json
 import pandas as pd
 
+
 def exec_jxa(filename, *args):
     filepath = os.path.join(os.path.dirname(__file__), 'jxa', filename)
     command = ['osascript', '-l', 'JavaScript', filepath, *args]
     return subprocess.check_output(command).decode()
 
+
 def fetch_track_list():
     return pd.read_json(exec_jxa('getAllTracks.js')).set_index('id')
 
+
 def play(df):
     exec_jxa('playTracks.js', json.dumps({'tracks': list(df.index)}))
+
+
+__all__ = ['fetch_track_list', 'play']
