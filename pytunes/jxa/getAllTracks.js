c8cb00bed9c7fa9509896fa5a6cd48f7d55fb090
hoto17296 <mail@hoto.me>
2019-08-17 10:04:18 +0900

    refactor

--- pytunes/jxa/getAllTracks.js
+++ pytunes/jxa/getAllTracks.js
@@ -1,15 +1,6 @@
-function fetchPlaylistTracks(playlist) {
-  const arr = [];
-  for (let i = 0; i < playlist.tracks.length; i++) {
-    const track = playlist.tracks[i];
-    arr.push(track.properties());
-  }
-  return arr;
-}
-
 function run() {
   const itunes = Application('iTunes');
   const music = itunes.playlists[1];
-  const tracks = fetchPlaylistTracks(music);
+  const tracks = music.tracks.properties();
   return JSON.stringify(tracks);
 }
